a5017c24a4a8a0ecacdff1d96ba377c3ef7b609c
Giovanny <diasgiovanny@gmail.com>
2023-03-30 16:51:03 +0200

    CRUD modif

--- backend/controllers/sauces.js
+++ backend/controllers/sauces.js
@@ -18,9 +18,9 @@ exports.getOneSauce = (req, res, next) => {
       .catch(error => res.status(404).json({ error }));
   };
   
+
 exports.createSauce = (req, res, next) => {
     const sauceObject = JSON.parse(req.body.sauce);
-    delete sauceObject._id;
     const sauce = new Sauce({
       ...sauceObject,
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`
@@ -28,8 +28,8 @@ exports.createSauce = (req, res, next) => {
     sauce.save()
       .then(() => res.status(201).json({ message: 'Sauce enregistrée !'}))
       .catch(error => res.status(400).json({ error }));
-};
-
+  };
+  
   
 exports.modifySauce = (req, res, next) => {
 const sauceObject = req.file ?
--- backend/middlewares/multer-config.js
+++ backend/middlewares/multer-config.js
@@ -1,4 +1,5 @@
 const multer = require('multer');
+const path = require('path');
 
 const MIME_TYPES = {
   'image/jpg': 'jpg',
@@ -13,7 +14,9 @@ const storage = multer.diskStorage({
   filename: (req, file, callback) => {
     const name = file.originalname.split(' ').join('_');
     const extension = MIME_TYPES[file.mimetype];
-    callback(null, name + Date.now() + '.' + extension);
+    const sauceId = req.body.sauceId;
+    const fileName = sauceId + '_' + Date.now() + '.' + extension;
+    callback(null, fileName);
   }
 });
 
